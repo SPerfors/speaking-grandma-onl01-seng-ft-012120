0332069238b13fe665104a7254aa4cb6a5a81c61
Steven Perfors <stevecanbuild@gmail.com>
2020-01-23 23:17:09 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,16 +13,10 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 string = "I LOVE YOU GRANDMA!"
-string = "BOB"
-string = "jujio"
+
 
 def speak_to_grandma(string)
-  if string == "I LOVE YOU GRANDMA!"
+  string == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
-  elsif string == string.upcase
-    puts "NO, NOT SINCE 1938!"
-  else
-    puts "HUH? SPEAK UP, SONNY!"
-  end
 end
 
